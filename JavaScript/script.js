f1f624f3fa4dd44ecfdfa8edb1c8eb853f603549
ReNayeem <renayeem@gmail.com>
2022-02-16 23:14:29 +0600

    calculate value input completed

---
+++ JavaScript/script.js
@@ -0,0 +1,26 @@
+// calculate button start
+document.getElementById('calculate-button').addEventListener('click', function () {
+
+    // income amount input start
+    const incomeAmountInput = document.getElementById('income-amount-input')
+    const incomeAmountValue = incomeAmountInput.value
+    // income amount input end
+
+    // food expense input start
+    const foodExpenseInput = document.getElementById('food-expense-input')
+    const foodExpenseValue = foodExpenseInput.value
+    // food expense input end
+
+    // rent expense input start
+    const rentExpenseInput = document.getElementById('rent-expense-input')
+    const rentExpenseValue = rentExpenseInput.value
+    // rent expense input end
+
+    // cloth expense input start
+    const clothExpenseInput = document.getElementById('cloth-expense-input')
+    const clothExpenseValue = clothExpenseInput.value
+    // cloth expense input end
+
+    // clear input value 
+    // incomeAmountInput.value = ''
+})
